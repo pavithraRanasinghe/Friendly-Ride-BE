3fdc611428c37382129b850786886f942d8570df
pavithraRanasinghe <paviranasinghe20@gmail.com>
2024-08-04 11:32:20 +0530

    Initialize Project

---
+++ src/main/java/com/esoft/friendlyride/FriendlyRideApplication.java
@@ -0,0 +1,13 @@
+package com.esoft.friendlyride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendlyRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendlyRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esoft/friendlyride/FriendlyRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esoft.friendlyride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendlyRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
